e4d62c34ad73f8d55e4bb149ab16d5af06b8bb3e
smqlyy <vodichka2006@gmail.com>
2026-08-16 21:00:41 +0500

    fix

--- backend/src/parsers/test.ts
+++ backend/src/parsers/test.ts
@@ -2,22 +2,10 @@ import { parseDNS } from "./DNS.parser";
 import { parseWB } from "./WB.parser";
 import { parseGEIZHALS } from "./GEIZHALS.parser";
 import {parsePCPARTKEEPER} from './PCPARTKEEPER.parser'
+import {parseCapsLocks} from './parse'
 
 //parseDNS('https://books.toscrape.com/').then(result=>{console.log(result)})
 //parseWB('https://www.wildberries.ru/catalog/835882175/detail.aspx').then(result=>{console.log(result)})
 //parseGEIZHALS('https://geizhals.de/gigabyte-gaming-a18-3whk3dec64sh-a3685733.html').then(result=>{console.log(result)})
 //parsePCPARTKEEPER('https://pcpartkeeper.com/models/1433').then(result=>{console.log(result)})
-https://capslocks.ru/klaviatury/tproduct/910381501242-klaviatura-wooting-60he-black
-
-
-
-const url = 'https://pcpartkeeper.com/models/1433'
-async function test() {
-    const result = await parsePCPARTKEEPER(url)
-    console.log('📦 Название:', result.name)
-    console.log('📁 Категория:', result.category)
-    console.log('💻 Chip:', result.chip)
-    console.log('📊 Статус:', result.status)
-}
-
-test()
+parseCapsLocks('https://capslocks.ru/klaviatury/tproduct/910381501242-klaviatura-wooting-60he-black').then(result=>{console.log(result)})
